c348d696dcc49069217b7aa5a9efdb14def3a581
ivanletralok <ivanletralok@gmail.com>
2023-02-06 23:36:19 -0500

    feat: implement particles js to home

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var particlesJS: any;
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  title="particle";
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    particlesJS.load('particles-js', 'assets/particles.json', () => {
+      console.log('callback - particles.js config loaded');
+    });
+
+  }
+
+
+
+}
